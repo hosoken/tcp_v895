1f4678b7b8d87e631971504c27db1cf7607cfcfb
hosoken <hosomi@lily03.intra.j-parc.jp>
2014-12-26 14:25:16 +0900

    Add timeout in socket connection
    Try Catch for error handling.
    If error, always close socket.
    Before closing socket, wait 1 sec.

--- v895.cc
+++ v895.cc
@@ -6,6 +6,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <fcntl.h>
 #include <fstream>
 
 #define A32 2
@@ -16,8 +17,9 @@
 #define D16 1
 #define D8  0
 
-int vme_write(int s,int am, int dm, unsigned int address, unsigned int value);
-int vme_read(int s, int am, int dm, unsigned int address, unsigned int* value);
+int vme_write(int sock,int am, int dm, unsigned int address, unsigned int value);
+int vme_read(int sock, int am, int dm, unsigned int address, unsigned int* value);
+int connect(int sock, const char *host, int port, double timeout_sec);
 
 int main(int argc, char *argv[])
 {
@@ -28,23 +30,9 @@ int main(int argc, char *argv[])
     exit(EXIT_FAILURE);
   }
 
-  int sock;
-  sock = socket(AF_INET, SOCK_STREAM, 0);
-  
-  char sitcpIpAddr[]="192.168.30.41";
-  unsigned int sitcpPort = 24;
-
-  struct sockaddr_in addr;
-  addr.sin_family       = AF_INET;
-  addr.sin_port         = htons(sitcpPort);
-  addr.sin_addr.s_addr  = inet_addr(sitcpIpAddr);
-
-  if(connect(sock, (struct sockaddr*)&addr, sizeof(addr))<0){
-    printf("Unable to connect %s : %d\n",sitcpIpAddr, sitcpPort);
-    close(sock);
-    exit(EXIT_FAILURE);
-  }
-  
+  int sock = socket(AF_INET, SOCK_STREAM, 0);
+  connect(sock, "192.168.30.41", 24, 0.5);    
+ 
   //read file and setting
   unsigned int vme_addr=0;
   std::string line;
@@ -107,6 +95,35 @@ int main(int argc, char *argv[])
 // internal functions
 /////////////////////////////////////////////////////////////////////////////////////////////
 
+int connect(int sock, const char *host, int port, double timeout_sec)
+{
+  struct timeval tv={(int)timeout_sec, (timeout_sec-(int)timeout_sec)*1000000.};
+  
+  struct sockaddr_in addr;
+  addr.sin_family       = AF_INET;
+  addr.sin_port         = htons(port);
+  addr.sin_addr.s_addr  = inet_addr(host);
+
+  fd_set set;
+  FD_ZERO(&set);
+  FD_SET(sock, &set);
+
+  int flags = fcntl(sock, F_GETFL, NULL);
+  fcntl(sock, F_SETFL, flags | O_NONBLOCK); 
+
+  connect(sock, (struct sockaddr*)&addr, sizeof(addr));
+
+  if(select(sock+1, NULL, &set, NULL, &tv) < 1){
+    printf("cannot connect to %s:%d\n",host,port);
+    close(sock);
+    exit(0);
+  }
+
+  fcntl(sock, F_SETFL, flags);
+  printf("#socket open\n");
+  return 0;
+}
+
 
 struct sitcp_vme_master_header{
   unsigned int address;
@@ -125,121 +142,116 @@ int vme_read(int sock, int am, int dm, unsigned int address, unsigned int* value
   unsigned char crc;
   unsigned char sndBuf[12];
   
-  if(am!=0 && am!=1 && am!=2){
-    printf("Unknown Address Mode\n");
-    return -1;
-  }
+  try
+    {
 
-  if(dm==0){
-    sndHeader.length=htonl(1);
-  }else if(dm==1){
-    sndHeader.length=htonl(2);
-  }else if(dm==2){
-    sndHeader.length=htonl(4);
-  }else{
-    printf("Unknown Data Mode\n");
-    return -1;
-  }
-  
-  sndHeader.address=htonl(address);
-  sndHeader.mode=htons(am<<8 | dm<<10);
-  sndHeader.id=1;
-  
-  memcpy(sndBuf, &sndHeader, sizeof(sndHeader));
+      if(am!=0 && am!=1 && am!=2){
+	printf("Unknown Address Mode: %d\n",am);
+	throw 1;
+      }
 
-  crc = 0xFF;
-  for(int i=0;i<11;i++) crc=crcCal(crc,sndBuf[i]);  
-  sndBuf[11]=crc;
+      if(dm==0){
+	sndHeader.length=htonl(1);
+      }else if(dm==1){
+	sndHeader.length=htonl(2);
+      }else if(dm==2){
+	sndHeader.length=htonl(4);
+      }else{
+	printf("Unknown Data Mode: %d\n",dm);
+	throw 1;
+      }
+  
+      sndHeader.address=htonl(address);
+      sndHeader.mode=htons(am<<8 | dm<<10);
+      sndHeader.id=1;
+  
+      memcpy(sndBuf, &sndHeader, sizeof(sndHeader));
 
-  //send CMD packet
-  if(send(sock, sndBuf, sizeof(sndHeader), 0)<0){
-    printf("send() failed\n");
-    close(sock);
-    exit(EXIT_FAILURE);
-  }
+      crc = 0xFF;
+      for(int i=0;i<11;i++) crc=crcCal(crc,sndBuf[i]);  
+      sndBuf[11]=crc;
 
-  unsigned char recvBuf[16];
-  struct sitcp_vme_master_header rcvHeader;
+      //send CMD packet
+      if(send(sock, sndBuf, sizeof(sndHeader), 0)<0){
+	printf("send() failed\n");
+	throw 1;
+      }
 
-  //receive ACK packet   
-  int rLen=0;
+      unsigned char recvBuf[16];
+      struct sitcp_vme_master_header rcvHeader;
+      
+      //receive ACK packet   
+      int rLen=0;
+      
+      rLen = receive(sock, (char*)&rcvHeader, sizeof(rcvHeader));
+      if(rLen != sizeof(rcvHeader)){
+	printf("Header receive() failed\n");
+	throw 1;
+      }
   
-  rLen = receive(sock, (char*)&rcvHeader, sizeof(rcvHeader));
-  if(rLen != sizeof(rcvHeader)){
-    printf("Header receive() failed\n");
-    close(sock);
-    exit(EXIT_FAILURE);
-  }
-  
-  memcpy(recvBuf, &rcvHeader, sizeof(rcvHeader));
-  
-  rcvHeader.address = ntohl(rcvHeader.address);
-  rcvHeader.length  = ntohl(rcvHeader.length);
-  rcvHeader.mode    = ntohs(rcvHeader.mode);
-  
-  //printf("   Address  = 0x%.8x\n", rcvHeader.address);
-  //printf("   Length   = 0x%.8x\n", rcvHeader.length);
-  //printf("   Mode     = 0x%.4x\n", rcvHeader.mode);
-  //printf("   ID       = 0x%.2x\n", rcvHeader.id);
-  //printf("   CRC      = 0x%.2x\n", rcvHeader.crc);
-   
-  //Caluculate the received CRC 
-  crc=0xFF;
-  for(int i=0; i<12; i++) crc=crcCal(crc, recvBuf[i]);
-  if(crc!=0){
-    printf("ACK CRC ERROR!! : %x (Hex)\n",crc);
-    close(sock);
-    exit(EXIT_FAILURE);
-  }
-  
-  //Error Check
-  if((rcvHeader.mode & 0x1)==0x1){
-    printf(" ----- Parameter Error !! -----\n");
-    close(sock);
-    exit(EXIT_FAILURE);
-  }
-  if((rcvHeader.mode & 0x4)==0x4){
-    printf(" ----- VME bus TIMEOUT !! -----\n");
-    close(sock);
-    exit(EXIT_FAILURE);
-  }
-  
-  //read VME data
-   if(dm==0){
-     rLen=0;
-     rLen = receive(sock, (char*)(recvBuf+12), 1);
-     if(rLen != 1){
-       printf("VME data receive() failed\n");
-       close(sock);
-       exit(EXIT_FAILURE);
-     }
-     *value = (unsigned int)recvBuf[12];
-     //printf("%.2x\n",recvBuf[12]);
-   }else if(dm==1){
-     rLen=0;
-     rLen = receive(sock, (char*)(recvBuf+12), 2);
-     if(rLen != 2){
-       printf("VME data receive() failed\n");
-       close(sock);
-       exit(EXIT_FAILURE);
-     }
-     *value = (unsigned int)(recvBuf[12]<<8 | recvBuf[13]);
-     //printf("%.2x %.2x\n",recvBuf[12],recvBuf[13]);
-   }else if(dm==2){
-     rLen=0;
-     rLen = receive(sock, (char*)(recvBuf+12), 4);
-     if(rLen != 4){
-       printf("VME data receive() failed\n");
-       close(sock);
-       exit(EXIT_FAILURE);
-     }
-     *value = (unsigned int)(recvBuf[12]<<24 | recvBuf[13]<<16 | recvBuf[14]<<8 | recvBuf[15]);
-     //printf("%.2x %.2x %.2x %.2x\n",recvBuf[12],recvBuf[13],recvBuf[14],recvBuf[15]);
-   }
-   
+      memcpy(recvBuf, &rcvHeader, sizeof(rcvHeader));
+      
+      rcvHeader.address = ntohl(rcvHeader.address);
+      rcvHeader.length  = ntohl(rcvHeader.length);
+      rcvHeader.mode    = ntohs(rcvHeader.mode);
+      
+      //Caluculate the received CRC 
+      crc=0xFF;
+      for(int i=0; i<12; i++) crc=crcCal(crc, recvBuf[i]);
+      if(crc!=0){
+	printf("ACK CRC ERROR!! : %x (Hex)\n",crc);
+	throw 1;
+      }
+      
+      //Error Check
+      if((rcvHeader.mode & 0x1)==0x1){
+	printf(" ----- Parameter Error !! -----\n");
+	throw 1;
+      }
+      if((rcvHeader.mode & 0x4)==0x4){
+	printf(" ----- VME bus TIMEOUT !! -----\n");
+	throw 1;
+      }
+      
+      //read VME data
+      if(dm==0){
+	rLen=0;
+	rLen = receive(sock, (char*)(recvBuf+12), 1);
+	if(rLen != 1){
+	  printf("VME data receive() failed\n");
+	  throw 1;
+	}
+	*value = (unsigned int)recvBuf[12];
+	
+      }else if(dm==1){
+	rLen=0;
+	rLen = receive(sock, (char*)(recvBuf+12), 2);
+	if(rLen != 2){
+	  printf("VME data receive() failed\n");
+	  throw 1;
+	}
+	*value = (unsigned int)(recvBuf[12]<<8 | recvBuf[13]);
+	
+      }else if(dm==2){
+	rLen=0;
+	rLen = receive(sock, (char*)(recvBuf+12), 4);
+	if(rLen != 4){
+	  printf("VME data receive() failed\n");
+	  throw 1;
+	}
+	*value = (unsigned int)(recvBuf[12]<<24 | recvBuf[13]<<16 | recvBuf[14]<<8 | recvBuf[15]);
+      }
+      
+    } 
+  catch(...)
+    {
+      sleep(1);
+      close(sock);
+      printf("#socket close\n");
+      exit(-1);
+    }
   return 0;
 }
-
 
 int vme_write(int sock, int am, int dm, unsigned int address, unsigned int value)
 {
@@ -247,107 +259,105 @@ int vme_write(int sock, int am, int dm, unsigned int address, unsigned int value
   unsigned char crc;
   unsigned char sndBuf[16];
   
+  try
+    {
 
-  if(am!=0 && am!=1 && am!=2){
-    printf("Unknown Address Mode\n");
-    return -1;
-  }
+      if(am!=0 && am!=1 && am!=2){
+	printf("Unknown Address Mode: %d\n",am);
+	throw 1;
+      }
 
-  if(dm==0){
-    sndHeader.length=htonl(1);
-  }else if(dm==1){
-    sndHeader.length=htonl(2);
-  }else if(dm==2){
-    sndHeader.length=htonl(4);
-  }else{
-    printf("Unknown Data Mode\n");
-    return -1;
-  }
+      if(dm==0){
+	sndHeader.length=htonl(1);
+      }else if(dm==1){
+	sndHeader.length=htonl(2);
+      }else if(dm==2){
+	sndHeader.length=htonl(4);
+      }else{
+	printf("Unknown Data Mode: %d\n",dm);
+	throw 1;
+      }
   
-  sndHeader.address=htonl(address);
-  sndHeader.mode=htons(0x1<<15 | am<<8 | dm<<10);
-  sndHeader.id=1;
+      sndHeader.address=htonl(address);
+      sndHeader.mode=htons(0x1<<15 | am<<8 | dm<<10);
+      sndHeader.id=1;
+      
+      memcpy(sndBuf, &sndHeader, sizeof(sndHeader));
+      
+      crc = 0xFF;
+      for(int i=0;i<11;i++) crc=crcCal(crc,sndBuf[i]);
+      
+      sndBuf[11]=crc;
+      
+      if(dm==0){
+	sndBuf[12]=(unsigned char)value;
+	if(send(sock, sndBuf, sizeof(sndHeader)+1, 0)<0){
+	  printf("send() failed\n");
+	  throw 1;
+	}
+      }else if(dm==1){
+	sndBuf[12]=(unsigned char)((value>>8) &0xFF);
+	sndBuf[13]=(unsigned char)( value &0xFF);
+	if(send(sock, sndBuf, sizeof(sndHeader)+2, 0)<0){
+	  printf("send() failed\n");
+	  throw 1;
+	}
+      }else if(dm==2){
+	sndBuf[12]=(unsigned char)((value>>24) &0xFF);
+	sndBuf[13]=(unsigned char)((value>>16) &0xFF);
+	sndBuf[14]=(unsigned char)((value>>8) &0xFF);
+	sndBuf[15]=(unsigned char)( value &0xFF);
+	if(send(sock, sndBuf, sizeof(sndHeader)+4, 0)<0){
+	  printf("send() failed\n");
+	  throw 1;
+	}
+      }
+      
+      unsigned char recvBuf[12];
+      struct sitcp_vme_master_header rcvHeader;
+      
+      //receive ACK packet
+      int rLen=0;
+      
+      rLen = receive(sock, (char*)&rcvHeader, sizeof(rcvHeader));
+      if(rLen != sizeof(rcvHeader)){
+	printf("Header receive() failed\n");
+	throw 1;
+      }
+      
+      memcpy(recvBuf, &rcvHeader, sizeof(rcvHeader));
+      
+      rcvHeader.address = ntohl(rcvHeader.address);
+      rcvHeader.length  = ntohl(rcvHeader.length);
+      rcvHeader.mode    = ntohs(rcvHeader.mode);
+      
+      //Caluculate the received CRC 
+      crc=0xFF;
+      for(int i=0; i<12; i++) crc=crcCal(crc, recvBuf[i]);
+      if(crc!=0){
+	printf("ACK CRC ERROR!! : %x (Hex)\n",crc);
+	throw 1;
+      }
   
-  memcpy(sndBuf, &sndHeader, sizeof(sndHeader));
-
-  crc = 0xFF;
-  for(int i=0;i<11;i++) crc=crcCal(crc,sndBuf[i]);
-  
-  sndBuf[11]=crc;
-
-  if(dm==0){
-    sndBuf[12]=(unsigned char)value;
-    if(send(sock, sndBuf, sizeof(sndHeader)+1, 0)<0){
-      printf("send() failed\n");
-      close(sock);
-      exit(EXIT_FAILURE);
+      //Error Check
+      if((rcvHeader.mode & 0x1)==0x1){
+	printf(" ----- Parameter Error !! -----\n");
+	throw 1;
+      }
+      if((rcvHeader.mode & 0x4)==0x4){
+	printf(" ----- VME bus TIMEOUT !! -----\n");
+	throw 1;
+      }
+      
     }
-  }else if(dm==1){
-    sndBuf[12]=(unsigned char)((value>>8) &0xFF);
-    sndBuf[13]=(unsigned char)( value &0xFF);
-    if(send(sock, sndBuf, sizeof(sndHeader)+2, 0)<0){
-      printf("send() failed\n");
+  catch(...)
+    {
+      sleep(1);
       close(sock);
-      exit(EXIT_FAILURE);
+      printf("#socket close\n");
+      exit(-1);
     }
-  }else if(dm==2){
-    sndBuf[12]=(unsigned char)((value>>24) &0xFF);
-    sndBuf[13]=(unsigned char)((value>>16) &0xFF);
-    sndBuf[14]=(unsigned char)((value>>8) &0xFF);
-    sndBuf[15]=(unsigned char)( value &0xFF);
-    if(send(sock, sndBuf, sizeof(sndHeader)+4, 0)<0){
-      printf("send() failed\n");
-      close(sock);
-      exit(EXIT_FAILURE);
-    }
-  }
 
-  unsigned char recvBuf[12];
-  struct sitcp_vme_master_header rcvHeader;
-  
-  //receive ACK packet
-  int rLen=0;
-  
-  rLen = receive(sock, (char*)&rcvHeader, sizeof(rcvHeader));
-  if(rLen != sizeof(rcvHeader)){
-    printf("Header receive() failed\n");
-    close(sock);
-    exit(EXIT_FAILURE);
-  }
-   
-  memcpy(recvBuf, &rcvHeader, sizeof(rcvHeader));
-  
-  rcvHeader.address = ntohl(rcvHeader.address);
-  rcvHeader.length  = ntohl(rcvHeader.length);
-  rcvHeader.mode    = ntohs(rcvHeader.mode);
-  
-  //printf("   Address  = 0x%.8x\n", rcvHeader.address);
-  //printf("   Length   = 0x%.8x\n", rcvHeader.length);
-  //printf("   Mode     = 0x%.4x\n", rcvHeader.mode);
-  //printf("   ID       = 0x%.2x\n", rcvHeader.id);
-  //printf("   CRC      = 0x%.2x\n", rcvHeader.crc);
-   
-  //Caluculate the received CRC 
-  crc=0xFF;
-  for(int i=0; i<12; i++) crc=crcCal(crc, recvBuf[i]);
-  if(crc!=0){
-    printf("ACK CRC ERROR!! : %x (Hex)\n",crc);
-    close(sock);
-    exit(EXIT_FAILURE);
-  }
-  
-  //Error Check
-  if((rcvHeader.mode & 0x1)==0x1){
-    printf(" ----- Parameter Error !! -----\n");
-    close(sock);
-    exit(EXIT_FAILURE);
-  }
-  if((rcvHeader.mode & 0x4)==0x4){
-    printf(" ----- VME bus TIMEOUT !! -----\n");
-    close(sock);
-    exit(EXIT_FAILURE);
-  }
-   
   return 0;
 }
 
